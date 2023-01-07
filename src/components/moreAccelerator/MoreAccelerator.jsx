3131ac4591d4b21075a29c025a28ac6c20726754
biksbee <77857837+biksbee@users.noreply.github.com>
2023-01-07 14:27:29 +0300

    added font-family

--- src/components/moreAccelerator/MoreAccelerator.jsx
+++ src/components/moreAccelerator/MoreAccelerator.jsx
@@ -34,7 +34,7 @@ const MoreAccelerator = () => {
                                 <SwiperSlide key={index} className='border-t-[2px] border-t-c_red-deep md:w-[488px]'>
                                     <div className={cn(
                                         'md:text-[45px] md:leading-[48px] text-[22px] leading-[25px] py-[30px]',
-                                        'font-otf not-italic font-semibold',
+                                        'font-otf-semiBold not-italic',
                                         index === 0 ? 'text-c_yellow-dirty' 
                                             : index === 1 ? 'text-c_orange-blur' 
                                             : index === 2 ? 'text-c_orange-bright' 
@@ -42,16 +42,16 @@ const MoreAccelerator = () => {
                                     )}>
                                         {item}
                                     </div>
-                                    <div className='md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] text-c_red-deep'>
+                                    <div className='md:text-[22px] md:leading-[30px] font-otf-book text-[15px] leading-[21px] text-c_red-deep'>
                                         {content.context[index]}
                                     </div>
                                     {content.needed[index] !== '' ? 
-                                        <div className='pt-[20px] opacity-[0.5] md:text-[16px] md:leading-[20px] text-[13px] leading-[16px]'>
+                                        <div className='pt-[20px] opacity-[0.5] md:text-[16px] md:leading-[20px] text-[13px] leading-[16px] uppercase font-otf-book'>
                                             требования
                                         </div>
                                     : null}
                                     <div className={cn(
-                                        'pt-[10px] md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] text-c_red-deep'
+                                        'pt-[10px] md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] text-c_red-deep font-otf-book'
                                     )}>
                                         {content.needed[index]}
                                     </div>
@@ -66,7 +66,7 @@ const MoreAccelerator = () => {
                                 <SwiperSlide key={index} className='border-t-[2px] border-t-c_red-deep pt-[20px] mt-[30px] md:w-[488px]'>
                                     <div className={cn(
                                         'md:text-[45px] md:leading-[48px] text-[22px] leading-[25px] pb-[30px]',
-                                        'font-otf not-italic font-semibold',
+                                        'font-otf-semiBold not-italic',
                                         index === 0 ? 'text-c_yellow-dirty' 
                                             : index === 1 ? 'text-c_orange-blur' 
                                             : index === 2 ? 'text-c_orange-bright' 
@@ -74,16 +74,16 @@ const MoreAccelerator = () => {
                                     )}>
                                         {item}
                                     </div>
-                                    <div className='md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] text-c_red-deep'>
+                                    <div className='md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] font-otf-book text-c_red-deep'>
                                         {content.context[index]}
                                     </div>
                                     {content.needed[index] !== '' ? 
-                                        <div className='pt-[20px] opacity-[0.5] md:text-[16px] md:leading-[20px] text-[13px] leading-[16px]'>
+                                        <div className='pt-[20px] opacity-[0.5] md:text-[16px] md:leading-[20px] text-[13px] leading-[16px] uppercase font-otf-semiBold'>
                                             требования
                                         </div>
                                     : null}
                                     <div className={cn(
-                                        'pt-[10px] md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] text-c_red-deep'
+                                        'pt-[10px] md:text-[22px] md:leading-[30px] text-[15px] leading-[21px] font-otf-book text-c_red-deep'
                                     )}>
                                         {content.needed[index]}
                                     </div>
@@ -101,12 +101,12 @@ const MoreAccelerator = () => {
                         0₽
                     </div>
                     <div className='flex justify-center pt-[9px]'>
-                        <div className='text-[21px] leading-[26px] font-otfBold font-bold not-italic text-center md:w-[169px]'>
+                        <div className='text-[21px] leading-[26px] font-otf-bold not-italic text-center md:w-[169px]'>
                             Стоимость участия
                         </div>
                     </div>
                     <div className='flex justify-center pt-[30px] md:pb-[60px] pb-[40px]'>
-                        <div className='w-[332px] md:text-[21px] md:leading-[30px] text-[15px] leading-[21px] font-otfNormal'>
+                        <div className='w-[332px] md:text-[21px] md:leading-[30px] text-[15px] leading-[21px] font-otf-book'>
                             Как для некоммерческих, так и для коммерческих проектов участие бесплатно.
                         </div>
                     </div>
@@ -115,11 +115,11 @@ const MoreAccelerator = () => {
                             <div 
                                 className={cn(
                                     'bg-c_orange-regular hover:bg-c_orange-hover active:bg-c_orange-pressed duration-500 flex justify-center items-center rounded-[6px]',
-                                    'w-[220px] h-[65px]',
+                                    'md:w-[230px] w-[173px] h-[65px]',
                                     'drop-shadow-default hover:drop-shadow-hover'
                                 )}
                             >
-                                <div className={cn('text-c_gray-regular cursor-pointer not-italic font-otf-book text-[18px] leading-[18px]')}>
+                                <div className={cn('text-[rgba(255,255,255,0.9)] cursor-pointer not-italic font-otf-semiBold md:text-[23px] text-[16px] md:leading-[23px] leading-[16px]')}>
                                     Подать заявку
                                 </div>
                             </div>
