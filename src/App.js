73813524d9496d25f9f7c4db730919f0769582a1
biksbee <77857837+biksbee@users.noreply.github.com>
2023-02-14 15:21:18 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import Load from './components/load'
-
 
 const App = () => {
   
   return (
     <div className="App">
       <Header />
-      {/* <Body /> */}
-      <Load />
+      <Body />
       <Footer />
     </div>
   );
